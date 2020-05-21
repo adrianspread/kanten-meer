5e82559d7ee96d6bed3b9c994954e5fa7441931e
Adrian Wysocki <dobrystuff77@gmail.com>
2020-05-21 16:49:06 +0200

    Toolbar

--- client/src/components/Toolbar/Toolbar.js
+++ client/src/components/Toolbar/Toolbar.js
@@ -2,9 +2,14 @@ import React, { Fragment } from "react";
 import classes from "./Toolbar.module.css";
 
 const toolbar = props => (
-  <Fragment>
-    <div className={classes.Toolbar}>TOOTLBAR</div>
-  </Fragment>
+  <>
+    <div className={classes.Toolbar}>
+      <div className={classes.ToolbarTop}>
+        <div className={classes.Language}></div>
+        <div className={classes.Login}>LOGIN</div>
+      </div>
+    </div>
+  </>
 );
 
 export default toolbar;
