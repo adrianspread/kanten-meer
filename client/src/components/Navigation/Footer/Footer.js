9e2860c84e92efa232721c6289356d426835a509
Adrian Wysocki <dobrystuff77@gmail.com>
2020-05-25 14:55:25 +0200

    Footer Sketch

---
+++ client/src/components/Navigation/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+import classes from "./Footer.module.css";
+
+const footer = () => <div className={classes.Footer}>Footer</div>;
+
+export default footer;
--- client/src/containers/Layout/Layout.js
+++ client/src/containers/Layout/Layout.js
@@ -1,15 +1,15 @@
-import React, { Fragment } from "react";
+import React from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import Main from "../../components/Main/Main";
+import Footer from "../../components/Navigation/Footer/Footer";
 
 const layout = props => (
-  <Fragment>
+  <>
     <Toolbar />
     <Main />
-  </Fragment>
+    <Footer />
+  </>
 );
 
 export default layout;
-
-// {props.children}
