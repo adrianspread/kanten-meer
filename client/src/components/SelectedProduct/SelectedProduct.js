49f318abfcaa3899ba567eca17fd8c69a7adc769
Adrian Wysocki <dobrystuff77@gmail.com>
2020-06-18 00:13:22 +0200

    Search Product

--- client/src/components/SelectedProduct/SelectedProduct.js
+++ client/src/components/SelectedProduct/SelectedProduct.js
@@ -45,28 +45,49 @@ class selectedProduct extends Component {
   render() {
     return (
       <div className={classes.Container}>
-        <div className={classes.ProductName}>
-          {this.state.product.map(product => {
+        {this.state.product &&
+          this.state.product.map(product => {
             return (
               <>
-                {product.board_supplier} &nbsp;
-                {product.material} &nbsp;
-                {product.ref_code}&nbsp; &nbsp;
+                <div className={classes.ProductName} key={product.ref_code}>
+                  <div>
+                    {product.board_supplier} &nbsp;
+                    {product.material} &nbsp;
+                    {product.ref_code}&nbsp; &nbsp;
+                  </div>
+                </div>
+                <div className={classes.ProductAndOrder}>
+                  <div className={classes.Product}>
+                    <div className={classes.ImageContiner}>
+                      <img src={url} alt="graphic" className={classes.Image} />
+                    </div>
+                  </div>
+                  <div className={classes.Order}>
+                    <button className={classes.Button}>LOGIN & ORDER</button>
+                  </div>
+                </div>
+                <div className={classes.Details}>Details</div>
+                <div className={classes.Description}>
+                  <div className={classes.Bold}>Description</div>
+                  <div className={classes.Text}>{product.description}</div>
+                  <div className={classes.Bold}>Reference Code</div>
+                  <div className={classes.Text}>{product.ref_code}</div>
+                  <div className={classes.Bold}>Board Supplier</div>
+                  <div className={classes.Text}>{product.board_supplier}</div>
+                  <div className={classes.Bold}>Base Material</div>
+                  <div className={classes.Text}>{product.base_material}</div>
+                  <div className={classes.Bold}>Model</div>
+                  <div className={classes.Text}>{product.model}</div>
+                  <div className={classes.Bold}>Processing Temperature</div>
+                  <div className={classes.Text}>
+                    {product.processing_temperature}C
+                  </div>
+                  <div className={classes.Bold}>Material</div>
+                  <div className={classes.Text}>{product.material}</div>
+                </div>
               </>
             );
           })}
-        </div>
-
-        <div className={classes.ProductAndOrder}>
-          <div className={classes.Product}>
-            <div className={classes.ImageContiner}>
-              <img src={url} alt="graphic" className={classes.Image} />
-            </div>
-          </div>
-          <div className={classes.Order}>
-            <button className={classes.Button}>LOGIN & ORDER</button>
-          </div>
-        </div>
       </div>
     );
   }
